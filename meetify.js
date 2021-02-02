9a7eadf394fad8a5b1a59a3259e1e3087560cf18
João <joao.guimaraes.alves.vaz@gmail.com>
2021-02-02 14:09:27 -0300

    Release v1.1; fix a bug

--- meetify.js
+++ meetify.js
@@ -1,4 +1,4 @@
 (function() {
-    if (window.location.query.includes('&authuser=1')) return;
+    if (window.location.search.includes('&authuser=1')) return;
     window.location.href += (window.location.search.includes('?') ? '&pli=1&authuser=1' : '?pli=1&authuser=1');
 })()
